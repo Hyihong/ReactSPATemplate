329d7708191602f144465475e5b70e9a0fd0e0a8
chenyihong <496695119@qq.com>
2017-07-25 10:32:36 +0800

    删除build文件夹

--- build/static/js/about.117d480a.chunk.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([4],{508:function(e,t,u){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var l=u(0),n=function(e){return e&&e.__esModule?e:{default:e}}(l),a=function(){return n.default.createElement("div",null,n.default.createElement("div",null,n.default.createElement("div",null,"About")))};t.default=a}});
-//# sourceMappingURL=about.117d480a.chunk.js.map
