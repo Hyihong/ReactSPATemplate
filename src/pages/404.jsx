c369762c576fd29fea8b9886bc860e5bc3c45473
chenyihong <496695119@qq.com>
2017-07-26 15:12:24 +0800

    修改页面切换 文档title

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
 class Home extends React.Component {
+  componentWillMount(){
+     document.title="404"
+  }
   render(){
     return (
       <div >
-        <div> 404 Page not Found</div>
+        <div> react work ,but 404 Page not Found</div>
       </div>
     );
   }
