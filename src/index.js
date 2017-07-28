d91436608a62281b3fa70cca07c00afc703e121d
chenyihong <496695119@qq.com>
2017-07-28 09:47:07 +0800

    将登入功能从App中分离

--- src/index.js
+++ src/index.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter as Router ,Route,Switch,Link } from 'react-router-dom';
+import {HashRouter as Router ,Route,Switch,Redirect } from 'react-router-dom';
 import { Provider } from 'react-redux'
 import NotFonund from './pages/404';
 
-import Routes from './Routes.js';
-//import Routes from './_Routes.jsx';
+import App from './App.js';
 import store from './Store'
 import './cover.less'
+import Login   from './pages/Login'
+
+import { LoginRoute }from './components/functional'
+
+// const Login = (props) => (
+//     <Bundle  load={(cb) => {
+//         require.ensure([], require => {
+//             cb(require('./pages/Login'));
+//         },'Login');
+//     }}>
+//         {(Login) => <Address {...Login}/>}
+//     </Bundle>
+// )
 
 ReactDOM.render(
     <Provider store={store}>
        <Router>
            <div>
-            <Switch>
-                <Route path="/404" component={NotFonund} />
-                <Route component={ Routes } />
-            </Switch>
+                <Switch>
+                    <Route path="/login" component = { Login }/> 
+                    <Route path="/404" component={NotFonund} />
+                    <LoginRoute  path="/" component={ App }/> 
+                </Switch>
             </div>
        </Router>
     </Provider>      
