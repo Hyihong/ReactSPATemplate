66e0e9867dd9e202d51b1ea116d6aca677d993e4
chenyihong <496695119@qq.com>
2017-07-28 09:56:27 +0800

    增加LOGIN木块reducer

--- src/Store.js
+++ src/Store.js
@@ -5,6 +5,7 @@ import { reducer as sharedReducer } from './components/shared'
 import { reducer as weatherReducer } from './components/weather'
 import { reducer as counterReducer } from './components/counter'
 import { reducer as articlesReducer } from './components/articles'
+import { reducer as loginReducer } from './components/loginForm'
 import resetEnhancer from './middleWare/reset.js' 
 import promise from './middleWare/promise' //中间件
 
@@ -20,7 +21,8 @@ const originalReducers = {
     shared: sharedReducer,
     weather: weatherReducer,
     counter: counterReducer,
-    articles: articlesReducer
+    articles: articlesReducer,
+    login: loginReducer
 }
 
 const reducer =  combineReducers(originalReducers)
