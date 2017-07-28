80427ec04223f05650860671c7deca5548fb0f74
chenyihong <496695119@qq.com>
2017-07-28 14:04:53 +0800

    美化登录页

--- src/components/loginForm/view.jsx
+++ src/components/loginForm/view.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom' ;
 import { loginin } from './actions';
-
+import './style.css'
 
 
 import { Form, Icon, Input, Button, Checkbox,Row,Col } from 'antd';
@@ -15,23 +15,38 @@ function LoginForm( {onLoginin,hasLogin} ) {
 
    if( !hasLogin){
        return (
-            <div style={{position:"absolute",width:'100%',height:"100%"}}>
-                <Row type="flex" justify="center" style={{height: "100%"}}>
-                    <Col >
-                        <Form style={{width: "300px"}}>
-                            <FormItem>
-                                <Input type="text" placeholder="请输入用户名"/>
-                            </FormItem>
-                            <FormItem>
-                                <Input type="password" placeholder="请输入密码"/>
-                            </FormItem>
-                            
-                            <Button type="primary" htmlType="submit" style={{width:"100%"}}  onClick ={ onLoginin }>
-                                登录
-                            </Button>
-                        </Form>
-                    </Col>
-                </Row>
+            <div className="containter">
+                <div className="bg"></div>
+                <div className="loginBox" >
+                    <Row type="flex" justify="center" align="middle" style={{height: "100%"}} >
+                        <Col style={{position:'relative'}}>
+                            <div className="loginPanel"></div>
+                            <Row type="flex" align="middle" style={{margin:"20px"}}>
+                                <Col style={{zIndex:11}}>
+                                    <div className="site">
+                                        <span>maybeLogo</span>
+                                    </div>
+                                </Col>
+                                <Col style={{zIndex:11}}>
+                                    <div className="loginFrom">
+                                        <Form  style={{width: "250px"}}>
+                                            <FormItem>
+                                                <Input type="text" size="large" placeholder="请输入用户名"/>
+                                            </FormItem>
+                                            <FormItem>
+                                                <Input type="password" size="large" placeholder="请输入密码"/>
+                                            </FormItem>
+                                            
+                                            <Button type="primary" htmlType="submit"   onClick ={ onLoginin }>
+                                                登录
+                                            </Button>
+                                        </Form>
+                                    </div>
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                </div>
             </div>
             
         );
