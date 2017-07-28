9fbbf0d770724d27510cccafae27714ac1c89b30
chenyihong <496695119@qq.com>
2017-07-28 09:56:13 +0800

    将顶栏和侧边栏重命名为Container

---
+++ src/pages/Container.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import  Header  from '../components/shared/Header';
+import  SilderMenu from '../components/shared/SilderMenu';
+import { Layout} from 'antd';
+const { Content } = Layout ;
+
+ class Container  extends React.Component{
+      componentWillMount(){
+      }
+      render(){
+           
+        return(
+          <div>
+            <Layout style={{width:"100%","minWidth":"1200px",height:'100%',position:'absolute'}} >
+                <Header></Header>
+                <Layout style={{width:"100%","minWidth":"1200px",height:'100%'}} className="ant-layout-has-sider">
+                    <SilderMenu current= {this.props.current}  openKeys= {this.props.openKeys}></SilderMenu>
+                    <Content style={{ padding: '0 5px 0 10px',margin:'10px 0 5px 0',overflow:'auto' }}>
+                         {this.props.children}
+                    </Content>
+                </Layout>
+            </Layout>
+          </div>)
+      }
+};
+
+export default Container ;
