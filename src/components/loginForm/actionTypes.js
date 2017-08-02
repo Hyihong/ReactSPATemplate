d406642aa8188b6cb8f52823eb4174e49219fdd1
chenyihong <496695119@qq.com>
2017-08-02 09:34:47 +0800

    【login】增加用户是否登录的action类型

--- src/components/loginForm/actionTypes.js
+++ src/components/loginForm/actionTypes.js
@@ -1,6 +1,7 @@
 //action类型,习惯用法 'type/'+type ，避免action类型冲突
 
-
+//export const LOGIN_HASLOGIN = "LOGIN/LOGIN_HASLOGIN" //判断用户是否已登入
+export const LOGIN_SET_STATUS ="LOGIN/SET_STATUS" // 记录用户状态
 
 export const LOGIN_START = 'LOGIN/LOGIN_START';     // 开始登入
 export const LOGIN_SUCCESS = 'LOGIN/LOGIN_SUCCESS'; // 登入成功
